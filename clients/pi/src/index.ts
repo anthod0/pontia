58586eb1bc33033d7a18348b8209fce1511a9a52
anthod0 <91065936+ocherry341@users.noreply.github.com>
2026-09-21 23:16:53 +0800

    chore(pi): migrate dependency to earendil Pi 0.86.1

--- clients/pi/src/index.ts
+++ clients/pi/src/index.ts
@@ -1,4 +1,4 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { defaultHookLogFile, loadTurnContext, type EnvLike, type LoadTurnContextResult, type TurnContext } from "./context.js";
 import { appendDiagnostic, type DiagnosticEntry } from "./diagnostics.js";
 import { pontiaHomeFromEnv, resolvePontiaConnection } from "./discovery.js";
